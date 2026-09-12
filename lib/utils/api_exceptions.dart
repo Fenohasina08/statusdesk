8666af6780d0ac552f2e03bf28b3deb335c1244e
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-09-12 10:46:34 +0300

    feat(exceptions): add API exception handling
    * Add ApiException base class
    * Add specific API exception classes
    * Add error messages for API failures

---
+++ lib/utils/api_exceptions.dart
@@ -0,0 +1,35 @@
+abstract class ApiException implements Exception {
+  final String message;
+
+  ApiException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class NoInternetException extends ApiException {
+  NoInternetException() : super('No internet connection.');
+}
+
+class TimeoutException_ extends ApiException {
+  TimeoutException_() : super('The server is taking too long to respond.');
+}
+
+class ServerException extends ApiException {
+  final int statusCode;
+
+  ServerException(this.statusCode)
+      : super('Server error ($statusCode).');
+}
+
+class NotFoundException extends ApiException {
+  NotFoundException() : super('Resource not found (404).');
+}
+
+class InvalidJsonException extends ApiException {
+  InvalidJsonException() : super('Invalid server response.');
+}
+
+class UnknownApiException extends ApiException {
+  UnknownApiException() : super('An unknown error occurred.');
+}
