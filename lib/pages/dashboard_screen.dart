bc75d1f48eb7d3418531e3e11bd06167e4f3a424
Justin.dev <justinbina515@gmail.com>
2026-09-15 22:29:59 +0000

    sync: use Accueil dashboard page

--- lib/pages/dashboard_screen.dart
+++ lib/pages/dashboard_screen.dart
@@ -1,49 +1,5 @@
- import 'package:flutter/material.dart';
-
-import 'Aceuil.dart';
+import 'package:flutter/material.dart';
+import 'Accueil.dart';
 import 'Services.dart';
-
-class DashboardScreen extends StatefulWidget {
-  const DashboardScreen({super.key});
-
-  @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
-  int _selectedIndex = 0;
-    List<Widget> pages = [
-      const Aceuil(),
-      const Services(),
-    ];
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Accueil',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Services',
-          ),
-        ],
-      ),
-    );
-  }
-}
+class DashboardScreen extends StatefulWidget { const DashboardScreen({super.key}); @override State<DashboardScreen> createState() => _DashboardScreenState(); }
+class _DashboardScreenState extends State<DashboardScreen> { int _selectedIndex = 0; final List<Widget> pages = [const Accueil(), const Services()]; void _onItemTapped(int index) => setState(() => _selectedIndex = index); @override Widget build(BuildContext context) => Scaffold(body: pages[_selectedIndex], bottomNavigationBar: BottomNavigationBar(currentIndex: _selectedIndex, onTap: _onItemTapped, type: BottomNavigationBarType.fixed, selectedItemColor: Colors.blue, unselectedItemColor: Colors.grey, selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold), unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal), items: const [BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil'), BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Services')])); }
