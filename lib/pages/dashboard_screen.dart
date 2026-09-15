ecdfc75ceb102830769129fffa07d4fe379578a0
draoude21 <201861475+justin2119@users.noreply.github.com>
2026-09-15 10:09:10 +0000

    Acceuil

--- lib/pages/dashboard_screen.dart
+++ lib/pages/dashboard_screen.dart
@@ -1,14 +1,49 @@
  import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
-   const DashboardScreen({super.key});
+import 'Aceuil.dart';
+import 'Services.dart';
 
-   @override
-   Widget build(BuildContext context) {
-     return  Scaffold(
-       appBar: AppBar(
-         title: const Text('Dashboard'),
-       ),
-     );
-   }
- }
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+    List<Widget> pages = [
+      const Aceuil(),
+      const Services(),
+    ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Services',
+          ),
+        ],
+      ),
+    );
+  }
+}
