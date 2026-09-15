f909b0cc81333ba35b55d0aa47ecfab60ef0dc56
Justin.dev <justinbina515@gmail.com>
2026-09-15 21:14:36 +0000

    fix: make Hive cache eager and resilient offline

--- lib/services/cache_service.dart
+++ lib/services/cache_service.dart
@@ -3,35 +3,24 @@ import 'package:hive/hive.dart';
 import '../models/service.dart';
 
 class CacheService {
-  static const String _servicesBoxName = 'services';
+  static const String servicesBoxName = 'services';
 
-  Future<Box<Service>> _getServicesBox() async {
-    if (Hive.isBoxOpen(_servicesBoxName)) {
-      return Hive.box<Service>(_servicesBoxName);
-    }
-
-    return Hive.openBox<Service>(_servicesBoxName);
+  static Future<Box<Service>> init() async {
+    if (Hive.isBoxOpen(servicesBoxName)) return Hive.box<Service>(servicesBoxName);
+    return Hive.openBox<Service>(servicesBoxName);
   }
+
+  Future<Box<Service>> _getServicesBox() => init();
 
   Future<void> saveServices(List<Service> services) async {
     final box = await _getServicesBox();
-
-    await box.clear();
-
-    for (final service in services) {
-      await box.put(service.name, service);
-    }
+    final nextNames = services.map((service) => service.name).toSet();
+    await box.putAll({for (final service in services) service.name: service});
+    final staleKeys = box.keys.where((key) => key is String && !nextNames.contains(key)).toList();
+    if (staleKeys.isNotEmpty) await box.deleteAll(staleKeys);
   }
 
-  Future<List<Service>> getCachedServices() async {
-    final box = await _getServicesBox();
+  Future<List<Service>> getCachedServices() async => (await _getServicesBox()).values.toList();
 
-    return box.values.toList();
-  }
-
-  Future<void> clearServices() async {
-    final box = await _getServicesBox();
-
-    await box.clear();
-  }
+  Future<void> clearServices() async => (await _getServicesBox()).clear();
 }
