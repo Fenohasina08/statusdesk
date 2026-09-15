f02a020c1e63fb957deff455bc6e40fe45d1a7a4
Justin.dev <justinbina515@gmail.com>
2026-09-15 22:29:56 +0000

    sync: initialize cache and service history

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'core/storage/hive_config.dart';
 import 'pages/dashboard_screen.dart';
 import 'providers/service_provider.dart';
 import 'repositories/service_repository.dart';
+import 'services/cache_service.dart';
 import 'services/service_api.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveConfig.init();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => ServiceProvider(
-        repository: ServiceRepository(api: ServiceApi()),
-      )
-        ..fetchServices()
-        ..startPolling(),
-      child: const MyApp(),
-    ),
-  );
+  await CacheService.init();
+  runApp(ChangeNotifierProvider(create: (_) => ServiceProvider(repository: ServiceRepository(api: ServiceApi()))..initialize()..startPolling(), child: const MyApp()));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'StatusDesk',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
-        home: const DashboardScreen(),
-      );
-}
+class MyApp extends StatelessWidget { const MyApp({super.key}); @override Widget build(BuildContext context) => MaterialApp(title: 'StatusDesk', debugShowCheckedModeBanner: false, theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true), home: const DashboardScreen()); }
