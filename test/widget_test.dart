faab5544b1fc973c3865c6de95313efd28ee5ce8
Justin.dev <justinbina515@gmail.com>
2026-09-16 11:41:17 +0000

    test: isolate widget test from Hive and fix duplicate Services assertion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,16 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:statusdesk/main.dart';
+import 'package:statusdesk/models/service.dart';
 import 'package:statusdesk/providers/service_provider.dart';
 import 'package:statusdesk/repositories/service_repository.dart';
 import 'package:statusdesk/services/service_api.dart';
 
-class FakeFailingHttpClient extends http.BaseClient {
+class FakeServiceRepository extends ServiceRepository {
+  FakeServiceRepository() : super(api: ServiceApi());
+
   @override
-  Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    throw const SocketException('Simulated network failure');
-  }
+  Future<List<Service>> getServices() async => <Service>[];
 }
 
 void main() {
@@ -21,16 +19,14 @@ void main() {
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (_) => ServiceProvider(
-          repository: ServiceRepository(
-            api: ServiceApi(client: FakeFailingHttpClient()),
-          ),
+          repository: FakeServiceRepository(),
         ),
         child: const MyApp(),
       ),
     );
 
     expect(find.text('StatusDesk'), findsOneWidget);
-    expect(find.text('Services'), findsOneWidget);
+    expect(find.text('Services'), findsNWidgets(2));
 
     await tester.pump();
   });
